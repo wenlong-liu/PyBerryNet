706279c11f5421a5cc1a235a0b1a5584399209fc
wliu14 <wenlongliu@yahoo.com>
2017-09-15 10:29:46 -0400

    Initial commit

---
+++ readme.md
@@ -0,0 +1,16 @@
+# PyBerryNet
+A Python API for BerryNet. 
+
+## Requirements
+
+* Raspberry Pi 3 Model(Or newer)
+          
+* Python libraries:
+    * [Paho-MQTT](https://pypi.python.org/pypi/paho-mqtt/1.3.0)
+    * [BerryNet](https://github.com/DT42/BerryNet)
+
+## Install
+
+## Usage
+
+## 
---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='PyBerryNet',
+    version='0.0.1',
+    packages=['PyBerryNet'],
+    url='',
+    license='GUN GPLv3',
+    author='Wenlong Liu',
+    author_email='wliu14@ncsu.edu',
+    description='A Python API for BerryNet: A Gateway for Deep Learning On Raspberry Pi'
+)
