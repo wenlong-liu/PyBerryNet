0e065ceac19e88a38a1ebb91ece49f313637b0a3
Wenlong Liu <wenlongliu@yahoo.com>
2017-09-28 23:54:51 -0400

    Update Example1_Recognize a local image.py

--- Examples/Example1_Recognize a local image.py
+++ Examples/Example1_Recognize a local image.py
@@ -1,6 +1,9 @@
 import pyberrynet
 
+# Start berrynet service.
 berrynet = pyberrynet.run()
+# Push a local file to server.
 results = berrynet.upload('Localimage', path='filepath')
 print(results)
-
+# Close the service.
+berrynet.close()
